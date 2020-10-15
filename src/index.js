fbd3ab9ff17f7109011f2fb788b24e62126607ac
danielofaustino <danieloliveirafaustino@gmail.com>
2020-10-14 22:42:23 -0300

    Metodo Post

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 const { response } = require('express')
 const express = require('express')
 const { uuid }= require('uuidv4')
+
 const app = express()
 
 app.use(express.json())
 
 const projects = []
 
+
 app.get('/projects', (req, res)=>{
+
     return res.json(projects);
+
 })
 
 app.post('/projects',(req, res) =>{
-    const {title, owner} = request.body;
+
+    const {title, owner} = req.body;
 
     const project = { id: uuid(), title, owner }
 
     projects.push(project)
 
-    return response.json(project);
+    return res.json(project);
 })
+
+
 
 
 app.listen(3333,() =>{
